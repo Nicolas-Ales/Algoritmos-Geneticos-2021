3e7d4227c534d21067d80ed1ee298903e8865365
Lucas <lcordoba844@gmail.com>
2021-09-09 19:01:35 -0300

    Eliminada doble definición de namedtuple "Capital"

--- TP3/Datos.py
+++ TP3/Datos.py
@@ -1,11 +1,5 @@
 from collections import namedtuple
 
-Capital = namedtuple('Capital',
-                     ['id', 'Nombre', 'd0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11',
-                      'd12', 'd13', 'd14', 'd15', 'd16', 'd17', 'd18', 'd19', 'd20', 'd21', 'd22', 'd23'])
-
-from collections import namedtuple
-
 Capital = namedtuple('Capital', ['id', 'Nombre', 'Distancias'])
 
 capitales = [
