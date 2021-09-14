8fbf7bdccb61680a10eaf495170bc02e965eccf6
Lucas <lcordoba844@gmail.com>
2021-09-14 18:54:58 -0300

    arreglado menú

--- TP3/Main.py
+++ TP3/Main.py
@@ -1,5 +1,5 @@
-from TP3.Datos import *
-from TP3.Funciones import *
+from Datos import *
+from Funciones import *
 
 
 nroPoblacion = 50
@@ -10,33 +10,32 @@ ruleta = True
 probCrossover = 0.5
 probMutacion = 0.5
 
-print('id\tNombre de la Capital')
-tabla = PrettyTable(['Capital', 'Id'])
-for c in capitales:
-    tabla.add_row([c.Nombre, c.id])
-print(tabla)
-seleccionCapital = int(input('Seleccione capital desde la que desea partir (por id): '))
-print('Seleccionó ', capitales[seleccionCapital].Nombre)
+
 print('Seleccione un método para la obtención del camino mínimo: ')
 print('1- Utilizar Método Exhaustivo')
-print('2- Utilizar Algoritmo Genético')
-print('3- Utilizar Método Heuristico')
+print('2- Utilizar Método Heuristico seleccionando ciudad')
+print('3- Camino mínimo con método heurístico.')
+print('4- Utilizar Algoritmo Genético')
 metodo = 0
 while metodo != 1 and metodo != 2 and metodo != 3:
     metodo = int(input())
 if metodo == 1:
     # resultado = Exhaustivo(capitales,seleccionCapital) #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado
-    print('Seleccionó Exhaustivo')
+    print('Seleccionó Exhaustivo y no podemos devolverle un resultado por este método')
 elif metodo == 2:
+    iDCapitalSeleccionada = SeleccionCapital(capitales)
+    resultado = Heuristico(capitales, iDCapitalSeleccionada)[0] #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado.
+elif metodo == 3:
+    resultado = CaminoMinimo(capitales) #resultado tiene que ser un recorrido de ids de capitales y la distancia minima.
+    print('Seleccionó Camino Mínimo con Heurístico.')
+elif metodo == 4:
     # resultado = Genetico(capitales,seleccionCapital) #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado
     # resultado = Genetico(capitales, nroPoblacion, nroCiclos, ruleta, elitismo, probCrossover, probMutacion)
     print('Seleccionó Genético')
-elif metodo == 3:
-    resultado1 = Heuristico(capitales,seleccionCapital) #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado
-    print('Seleccionó Heurístico')
 
-print(resultado1) #Print de prueba para el metodo Heurístico 
 
-resultado = list(range(24))  # Esta es una lista de prueba para testear el muestra datos
-resultado.append(resultado[0])  # Con esto resolvemos el que tenga qu evolver, al menos para la parte visual
-MuestraDatos(resultado, capitales)
+print(resultado) #Print de prueba para el metodo Heurístico 
+
+# resultado = list(range(24))  # Esta es una lista de prueba para testear el muestra datos
+# resultado.append(resultado[0])  # Con esto resolvemos el que tenga que volver, al menos para la parte visual
+# MuestraDatos(resultado, capitales)
