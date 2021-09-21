e92b086dbe5871c9fd90ab909723df162dc9542d
Nicolas <nicolas.ales@outlook.com>
2021-09-21 14:54:45 -0300

    cambio en el menu

--- TP3/Main.py
+++ TP3/Main.py
@@ -11,23 +11,19 @@ probMutacion = 0.05
 
 
 print('Seleccione un método para la obtención del camino mínimo: ')
-print('1- Utilizar Método Exhaustivo')
+print('1- Camino mínimo con método heurístico')
 print('2- Utilizar Método Heuristico seleccionando ciudad')
-print('3- Camino mínimo con método heurístico.')
-print('4- Utilizar Algoritmo Genético')
+print('3- Utilizar Algoritmo Genético')
 metodo = 0
 while metodo != 1 and metodo != 2 and metodo != 3 and metodo != 4:
     metodo = int(input())
-if metodo == 1:
-    # resultado = Exhaustivo(capitales,seleccionCapital) #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado
-    print('Seleccionó Exhaustivo y no podemos devolverle un resultado por este método')
-elif metodo == 2:
+if metodo == 2:
     iDCapitalSeleccionada = SeleccionCapital(capitales)
     resultado = Heuristico(capitales, iDCapitalSeleccionada)[0] #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado.
-elif metodo == 3:
+elif metodo == 1:
     resultado = CaminoMinimo(capitales) #resultado tiene que ser un recorrido de ids de capitales y la distancia minima.
     print('Seleccionó Camino Mínimo con Heurístico.')
-elif metodo == 4:
+elif metodo == 3:
     # resultado = Genetico(capitales,seleccionCapital) #resultado tiene que ser una lista con el id de las capitales en el orden seleccionado
     print('Seleccionó Genético')
     resultado = Genetico(capitales, nroPoblacion, nroCiclos, elitismo, probCrossover, probMutacion)
